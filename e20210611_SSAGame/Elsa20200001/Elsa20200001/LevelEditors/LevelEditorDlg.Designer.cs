bcfd4e3cab03901653e0e5e7067acd7a7a054339
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-09-10 09:39:11 +0900

    Voyager 1 is 23033667250.327 kilometers away from the Sun.

--- e20210611_SSAGame/Elsa20200001/Elsa20200001/LevelEditors/LevelEditorDlg.Designer.cs
+++ e20210611_SSAGame/Elsa20200001/Elsa20200001/LevelEditors/LevelEditorDlg.Designer.cs
@@ -30,14 +30,14 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LevelEditorDlg));
 			this.タイルGroup = new System.Windows.Forms.GroupBox();
+			this.TileMember = new System.Windows.Forms.ComboBox();
 			this.TileGroup = new System.Windows.Forms.ComboBox();
 			this.敵Group = new System.Windows.Forms.GroupBox();
+			this.EnemyMember = new System.Windows.Forms.ComboBox();
 			this.EnemyGroup = new System.Windows.Forms.ComboBox();
 			this.ShowTile = new System.Windows.Forms.CheckBox();
 			this.ShowEnemy = new System.Windows.Forms.CheckBox();
 			this.TileEnemySw = new System.Windows.Forms.Button();
-			this.TileMember = new System.Windows.Forms.ComboBox();
-			this.EnemyMember = new System.Windows.Forms.ComboBox();
 			this.タイルGroup.SuspendLayout();
 			this.敵Group.SuspendLayout();
 			this.SuspendLayout();
@@ -56,6 +56,19 @@
 			this.タイルGroup.Text = "タイル";
 			this.タイルGroup.Enter += new System.EventHandler(this.タイルGroup_Enter);
 			// 
+			// TileMember
+			// 
+			this.TileMember.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TileMember.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.TileMember.FormattingEnabled = true;
+			this.TileMember.Location = new System.Drawing.Point(6, 60);
+			this.TileMember.Name = "TileMember";
+			this.TileMember.Size = new System.Drawing.Size(348, 28);
+			this.TileMember.TabIndex = 1;
+			this.TileMember.SelectedIndexChanged += new System.EventHandler(this.TileMember_SelectedIndexChanged);
+			this.TileMember.Click += new System.EventHandler(this.TileClick);
+			// 
 			// TileGroup
 			// 
 			this.TileGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -67,7 +80,7 @@
 			this.TileGroup.Size = new System.Drawing.Size(348, 28);
 			this.TileGroup.TabIndex = 0;
 			this.TileGroup.SelectedIndexChanged += new System.EventHandler(this.TileGroup_SelectedIndexChanged);
-			this.TileGroup.Click += new System.EventHandler(this.Tile_Click);
+			this.TileGroup.Click += new System.EventHandler(this.TileClick);
 			// 
 			// 敵Group
 			// 
@@ -83,6 +96,19 @@
 			this.敵Group.Text = "敵 / イベントオブジェクト";
 			this.敵Group.Enter += new System.EventHandler(this.敵Group_Enter);
 			// 
+			// EnemyMember
+			// 
+			this.EnemyMember.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.EnemyMember.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.EnemyMember.FormattingEnabled = true;
+			this.EnemyMember.Location = new System.Drawing.Point(6, 60);
+			this.EnemyMember.Name = "EnemyMember";
+			this.EnemyMember.Size = new System.Drawing.Size(348, 28);
+			this.EnemyMember.TabIndex = 1;
+			this.EnemyMember.SelectedIndexChanged += new System.EventHandler(this.EnemyMember_SelectedIndexChanged);
+			this.EnemyMember.Click += new System.EventHandler(this.EnemyClick);
+			// 
 			// EnemyGroup
 			// 
 			this.EnemyGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -94,7 +120,7 @@
 			this.EnemyGroup.Size = new System.Drawing.Size(348, 28);
 			this.EnemyGroup.TabIndex = 0;
 			this.EnemyGroup.SelectedIndexChanged += new System.EventHandler(this.EnemyGroup_SelectedIndexChanged);
-			this.EnemyGroup.Click += new System.EventHandler(this.Enemy_Click);
+			this.EnemyGroup.Click += new System.EventHandler(this.EnemyClick);
 			// 
 			// ShowTile
 			// 
@@ -134,30 +160,6 @@
 			this.TileEnemySw.Text = "準備しています...";
 			this.TileEnemySw.UseVisualStyleBackColor = true;
 			this.TileEnemySw.Click += new System.EventHandler(this.TileEnemySw_Click);
-			// 
-			// TileMember
-			// 
-			this.TileMember.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.TileMember.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.TileMember.FormattingEnabled = true;
-			this.TileMember.Location = new System.Drawing.Point(6, 60);
-			this.TileMember.Name = "TileMember";
-			this.TileMember.Size = new System.Drawing.Size(348, 28);
-			this.TileMember.TabIndex = 1;
-			this.TileMember.SelectedIndexChanged += new System.EventHandler(this.TileMember_SelectedIndexChanged);
-			// 
-			// EnemyMember
-			// 
-			this.EnemyMember.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.EnemyMember.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.EnemyMember.FormattingEnabled = true;
-			this.EnemyMember.Location = new System.Drawing.Point(6, 60);
-			this.EnemyMember.Name = "EnemyMember";
-			this.EnemyMember.Size = new System.Drawing.Size(348, 28);
-			this.EnemyMember.TabIndex = 1;
-			this.EnemyMember.SelectedIndexChanged += new System.EventHandler(this.EnemyMember_SelectedIndexChanged);
 			// 
 			// LevelEditorDlg
 			// 
